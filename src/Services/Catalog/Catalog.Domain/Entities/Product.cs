f322c04eb11ab7c10d315d0e7460f70280fcd2fd
Gustavo Faria <gustavosh31@gmail.com>
2022-10-11 15:59:32 -0300

    Creating product class

---
+++ src/Services/Catalog/Catalog.Domain/Entities/Product.cs
@@ -0,0 +1,65 @@
+﻿using Catalog.Domain.Common;
+
+namespace Catalog.Domain.Entities;
+
+public class Product : Entity
+{
+    private int _quantity;
+    private string? _title;
+    private decimal _price;
+
+    public string? Description { get; private set; }
+
+    public string? Title 
+    { 
+        get { return _title; } 
+        private set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new Exception("Title is required");
+        }
+    }
+
+    public int Quantity
+    {
+        get { return _quantity; }
+        private set
+        {
+            if (value < 0)
+                throw new Exception("Quantity must be greater than zero");
+
+            _quantity = value;
+        }
+    }
+
+    public decimal Price
+    {
+        get { return _price; }
+        private set 
+        {
+            if (value < 0)
+                throw new Exception("Price must be greater than zero");
+
+            _price = value;
+        }
+    }
+
+    public Product()
+    {
+    }
+
+    public Product(string title, string description, int quantity)
+    {
+        Title = title;
+        Description = description;
+        Quantity = quantity;
+    }
+
+    public void ProductSold(int quantity)
+    {
+        if (quantity <= 0)
+            throw new Exception("Quantity must be greater than zero");
+
+        Quantity -= quantity;
+    }
+}
