d79aecac68d164a07c986e0c52327c675aeed190
Gustavo Faria <gustavosh31@gmail.com>
2022-10-15 17:15:33 -0300

    Creating tests for product entity

---
+++ src/Services/Catalog/Catalog.Test/Application/Entities/ProductTests.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Domain.Entities;
+
+namespace Catalog.Test.Application.Entities;
+
+public class ProductTests
+{
+    [Test]
+    [TestCase("")]
+    [TestCase(null)]
+    [TestCase("   ")]
+    public void InstantiateProduct_TitleIsInvalid_ThrowAnException(string title)
+    {
+        Assert.That(() => new Product(title, "", 10, 1), Throws.Exception);
+    }
+
+    [Test]
+    [TestCase(-3)]
+    [TestCase(-10)]
+    [TestCase(-1)]
+    public void InstantiateProduct_QuantityIsLessThanZero_ThrowAnException(int quantity)
+    {
+        Assert.That(() => new Product("Product", "", quantity, 1), Throws.Exception);
+    }
+
+    [Test]
+    [TestCase(-3)]
+    [TestCase(-10)]
+    [TestCase(-1)]
+    public void InstantiateProduct_PriceIsLessThanZero_ThrowAnException(decimal price)
+    {
+        Assert.That(() => new Product("Product", "", 1, price), Throws.Exception);
+    }
+}
