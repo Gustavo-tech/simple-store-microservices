148497a0e1cfb84a344976b4cc8a353c6780e344
Gustavo Faria <gustavosh31@gmail.com>
2022-10-20 21:11:10 -0300

    Starting configuring mass transit

---
+++ src/Services/Cart/Cart.Application/Features/Commands/Checkout/CheckouCommand.cs
@@ -0,0 +1,18 @@
+﻿using Cart.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Application.Features.Commands.Checkout;
+public class CheckouCommand : IRequest<CartEntity>
+{
+    public string Username { get; private set; }
+
+    public CheckouCommand(string username)
+    {
+        Username = username;
+    }
+}
---
+++ src/Services/Cart/Cart.Application/Features/Commands/Checkout/CheckoutCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using Cart.Application.Abstractions;
+using Cart.Application.Features.Commands.CreateCart;
+using Cart.Domain.Entities;
+using EventBus.Messages;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Application.Features.Commands.Checkout;
+
+public class CheckoutCommandHandler : IRequestHandler<CheckouCommand, CartEntity>
+{
+    private readonly ICartRepository _cartRepository;
+    private readonly ILogger<CreateCartCommandHandler> _logger;
+    private readonly IBus _bus;
+
+    public CheckoutCommandHandler(ICartRepository cartRepository, ILogger<CreateCartCommandHandler> logger, IBus bus)
+    {
+        _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _bus = bus ?? throw new ArgumentNullException(nameof(bus));
+    }
+
+    public async Task<CartEntity> Handle(CheckouCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            CartEntity cart = await _cartRepository.GetCartAsync(request.Username);
+            CartItem[] items = new CartItem[cart.Products.Count];
+
+            for (int i = 0; i <= cart.Products.Count; i++)
+            {
+                items[i] = new(cart.Products[i].ProductId, cart.Products[i].Quantity);
+            }
+
+            cart.Checkout();
+
+            await _cartRepository.UpdateCartAsync(cart);
+            await _bus.Publish(new CartCheckoutEvent(request.Username, items));
+
+            return cart;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"An error ocurred while checking out, {e.Message}");
+            throw;
+        }
+    }
+}
