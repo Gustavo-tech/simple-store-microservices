b8d28a57c75b48b2d37ec08272c1349b9bfb4f9d
Gustavo Faria <gustavosh31@gmail.com>
2022-10-24 16:46:39 -0300

    Writing tests for cart microservice

---
+++ src/Services/Cart/Cart.Test/Features/RemoveAllProductsCommandHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using Cart.Application.Abstractions;
+using Cart.Application.Features.Commands.CreateCart;
+using Cart.Application.Features.Commands.RemoveAllProducts;
+using Cart.Domain.Entities;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Test.Features;
+public class RemoveAllProductsCommandHandlerTests
+{
+    private readonly Mock<ICartRepository> _mockcartRepository = new();
+    private readonly Mock<ILogger<CreateCartCommandHandler>> _mockLogger = new();
+    private readonly RemoveAllProductsCommandHandler _commandHandler;
+
+    public RemoveAllProductsCommandHandlerTests()
+    {
+        _commandHandler = new(_mockcartRepository.Object, _mockLogger.Object);
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        List<CartProduct> cartProducts = new()
+        {
+            new CartProduct(1, 10),
+            new CartProduct(2, 15),
+            new CartProduct(3, 1),
+        };
+
+        _mockcartRepository.Setup(x => x.GetCartAsync(It.IsAny<string>()))
+            .ReturnsAsync(new CartEntity("Gustavo", cartProducts));
+    }
+
+    [Test]
+    public void TestHandle_UserNameIsValid_RemoveAllTheProducts()
+    {
+        RemoveAllProductsCommand command = new() { ProductId = 1, Username = "Gustavo" };
+        CancellationTokenSource source = new();
+
+        Assert.DoesNotThrowAsync(() => _commandHandler.Handle(command, source.Token));
+        _mockcartRepository.Verify(x => x.GetCartAsync(It.IsAny<string>()), Times.Once);
+        _mockcartRepository.Verify(x => x.UpdateCartAsync(It.Is<CartEntity>(x => x.Products.Count == 2)), Times.Once);
+    }
+}
---
+++ src/Services/Cart/Cart.Test/Features/RemoveProductCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using Cart.Application.Abstractions;
+using Cart.Application.Features.Commands.CreateCart;
+using Cart.Application.Features.Commands.RemoveProduct;
+using Cart.Domain.Entities;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Cart.Test.Features;
+public class RemoveProductCommandHandlerTests
+{
+    private Mock<ICartRepository> _mockcartRepository = new();
+    private Mock<ILogger<CreateCartCommandHandler>> _mockLogger = new();
+    private readonly RemoveProductCommandHandler _commandHandler;
+
+    public RemoveProductCommandHandlerTests()
+    {
+        _commandHandler = new(_mockcartRepository.Object, _mockLogger.Object);
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        List<CartProduct> cartProducts = new()
+        {
+            new CartProduct(1, 10),
+            new CartProduct(2, 15),
+            new CartProduct(3, 20),
+        };
+
+        _mockcartRepository.Setup(x => x.GetCartAsync(It.IsAny<string>()))
+            .ReturnsAsync(new CartEntity("Gustavo", cartProducts));
+    }
+
+    [Test]
+    [TestCase(1, 5, 5)]
+    [TestCase(2, 12, 3)]
+    [TestCase(3, 10, 10)]
+    public void TestHandle_UserNameIsValid_RemoveProducts(int productId, int quantityToRemove, int quantityExpected)
+    {
+        RemoveProductCommand command = new() { ProductId = productId, Username = "Gustavo", Quantity = quantityToRemove };
+        CancellationTokenSource source = new();
+
+        Assert.DoesNotThrowAsync(() => _commandHandler.Handle(command, source.Token));
+        _mockcartRepository.Verify(x => x.GetCartAsync(It.IsAny<string>()), Times.AtMost(3));
+        _mockcartRepository.Verify(x => x.UpdateCartAsync(It.Is<CartEntity>(x =>
+            x.Products.FirstOrDefault(x => x.ProductId == productId).Quantity == quantityExpected)), Times.AtMost(3));
+    }
+}
